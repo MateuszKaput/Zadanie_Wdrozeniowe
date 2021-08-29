b2047fab90d08e8aa19053f6e4846463f59664ae
Mateusz Kaput <kaput.inf@gmail.com>
2021-08-29 22:48:45 +0200

    Zmiana obrazków co 5s

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "../App.css";
 import "./HeroSection.css";
-import Img1 from "../assets/images/img-7.jpg";
-import Fact from "./Fact";
+import Kittens from "./Kittens";
 
 const fb =
   "https://lh3.googleusercontent.com/proxy/JcuAQpoFIJYYMnUVscZtCt5TGMyGkp6glAi0fmJ2HcdZDq5aRjHdkxRkAToWJB-ZvuLXTSWUTSr7EOFkF3O4yzOQLxes8fJ6vGAgr6COURUOH6FhrROs";
@@ -10,7 +9,6 @@ const insta =
   "https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-png-instagram-logo-camel-productions-website-25.png";
 const yt =
   "https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png";
-//const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
 const loremIpsum =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inv reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
@@ -19,10 +17,7 @@ function HeroSection() {
     <>
       <div className="hero-container">
         <div className="head-text">
-          <div className="head-image">
-            <img className="top-img" src={Img1} alt="Wops" />
-          </div>
-          <Fact />
+          <Kittens />
         </div>
 
         <div className="who_we_are">
---
+++ src/components/Kittens.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Kittens.css";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+
+class Kittens extends React.Component {
+  constructor() {
+    super();
+    this.slideRef = React.createRef();
+    this.state = {
+      current: 0,
+      text: "Female cats tend to be right pawed, while male cats are more often left pawed. Interestingly, while 90% of humans are right handed, the remaining 10% of lefties also tend to be male.",
+    };
+  }
+  next() {
+    this.slideRef.current.goNext();
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.changeKittiFact(), 6000);
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+  changeKittiFact() {
+    fetch("https://catfact.ninja/fact")
+      .then((res) => res.json())
+      .then((fact) =>
+        this.setState({
+          text: fact.fact,
+        })
+      );
+  }
+
+  render() {
+    const properties = {
+      duration: 5000,
+      autoplay: true,
+      transitionDuration: 1000,
+      arrows: false,
+      infinite: true,
+      easing: "ease",
+    };
+    const slideImages = [
+      "https://images3.alphacoders.com/567/thumb-1920-567149.jpg",
+      "https://images3.alphacoders.com/793/thumb-1920-793646.jpg",
+      "https://s1.1zoom.me/b5050/142/Cats_Kittens_Fence_Glance_576458_1920x1080.jpg",
+      "https://images7.alphacoders.com/640/thumb-1920-640360.jpg",
+      "https://www.tapeteos.pl/data/media/170/big/zwierzeta_1920x1080_054_koty.jpg",
+      "https://images.alphacoders.com/442/thumb-1920-442601.jpg",
+      "https://lh3.googleusercontent.com/proxy/xxalu3nV7br0tQNxdS2i-KHK23KZqXFkRbnMAkq85-OR6nC5g-P0Y25_5ZY4KJP5yJG_gyWAdkUlUGfBxV-yacep_lKUSWYwXPMvFSai0JDbiEso5De58MuAjoY",
+      "https://www.tapeteos.pl/data/media/170/big/zwierzeta_1920x1080_020_kot.jpg",
+      "https://s1.1zoom.me/b5050/747/Cats_Munchkin_Kittens_Cute_Four_4_566399_1920x1080.jpg",
+      "https://images.alphacoders.com/505/thumb-1920-505009.jpg",
+    ];
+    return (
+      <>
+        <div className="slide-container">
+          <Slide ref={this.slideRef} {...properties}>
+            {slideImages.map((each, index) => (
+              <div key={index} className="each-slide">
+                <img className="lazy" src={each} alt="sample" />
+              </div>
+            ))}
+          </Slide>
+        </div>
+        <div className="text-on-image">
+          <p>{this.state.text}</p>
+        </div>
+      </>
+    );
+  }
+}
+export default Kittens;
